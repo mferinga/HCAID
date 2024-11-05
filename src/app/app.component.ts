18ffa21843c33c181285d3836ef9c805acf1d413
Matthijs Feringa <mt.feringa@student.avans.nl>
2024-11-06 00:58:38 +0100

    made basic layout for the webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FeaturesModule } from './feature.module';
 
 @Component({
+  standalone: true,
+  imports: [RouterModule, FeaturesModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'HCAID-Project-Group-16';
 }
